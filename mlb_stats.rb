684dd65677a51dfafbb1ee39488636b527cf0489
Andy Holt <andyholt85@gmail.com>
2020-07-18 20:03:33 -0600

    added .gitignore and a few more stats to mlb_stats.rb

--- mlb_stats.rb
+++ mlb_stats.rb
@@ -5,6 +5,7 @@ require 'byebug'
 def scraper
   url = "https://www.baseball-reference.com/leagues/MLB/2019-standard-pitching.shtml"
   doc = Nokogiri::HTML(HTTParty.get(url))
+  teams = []
   
   doc.css('table.stats_table tr').each do |row|
     team_name = row.css('th.left a').text
@@ -12,14 +13,22 @@ def scraper
     
     wins = row.css("[data-stat = 'W']").text
     losses = row.css("[data-stat = 'L']").text
+    era = row.css("[data-stat = 'earned_run_avg']").text
+    hits_allowed = row.css("[data-stat = 'H']").text
+    homers_allowed = row.css("[data-stat = 'HR']").text
     
     team = {
       team_name: team_name,
       wins: wins,
-      losses: losses
+      losses: losses,
+      era: era,
+      hits_allowed: hits_allowed,
+      homers_allowed: homers_allowed
     }
-    puts team
+    teams << team
   end
+  #byebug
+  puts teams
 end
 
 scraper
